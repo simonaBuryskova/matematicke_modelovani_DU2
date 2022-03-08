389d44a0546a10af8ab8fba93c1ed485c23b2a6d
Jan Hrubeš <janhrubes73@seznam.cz>
2022-03-08 13:12:38 +0100

    Ještě malá oprava

--- main.m
+++ main.m
@@ -17,7 +17,7 @@
 (* ::Input:: *)
 (*(* Tayloruv rozvoj integrandu *)*)
 (*Integrand = 1/Sqrt[1-k^2*Sin[\[CurlyPhi]]^2];*)
-(*seriesIntegrand = Series[Integrand, {\[Theta]init, 0,2}]*)
+(*seriesIntegrand = Series[Integrand, {\[Theta]init, 0,4}]   (*Dle toho \[UAcute]kolu m\[AAcute]me rov\[IAcute]jet do jednoho \[RHacek]\[AAcute]du nav\[IAcute]c.*)*)
 
 
 (* ::Input:: *)
@@ -28,6 +28,9 @@
 (*TseriesAprox = 4*Sqrt[l/g]*Integrate[seriesIntegrand, {\[CurlyPhi], 0, \[Pi]/2}]*)
 
 
+TseriesAprox /. \[Theta]init->10/(2\[Pi]) /. g->9.81 /. l->1
+
+
 (* Koeficient u \[Theta]^2 *)
 (* SeriesCoefficient[TseriesAprox,{\[Theta]init,0,2}] *)
 
@@ -36,18 +39,21 @@
 TIntegralExplicit = 4*Sqrt[l/g]Integrate[1/Sqrt[1-k^2*Sin[\[CurlyPhi]]^2], {\[CurlyPhi], 0 , \[Pi]/2}]
 
 
+TIntegralExplicit /. \[Theta]init->10/(2\[Pi]) /. g->9.81 /. l->1
+
+
 (* ::Section:: *)
 (*Numericky*)
 
 
-g = 9.81;
-l = 1;
+konstg = 9.81;
+konstl = 1;
 konst\[Theta]init = 10/(2\[Pi]);    (*Tady se dosazovalo do glob\[AAcute]ln\[IAcute] prom\[EHacek]nn\[EAcute], dle kter\[EAcute] se neho\[RHacek]e rozv\[IAcute]j\[IAcute] a pak to d\[EHacek]lalo blbosti, je lep\[SHacek]\[IAcute] to pojmenovat jinak*)
 \[Omega]init = 0;
 sol = Reap[ (* "Reap" posb\[IAcute]r\[AAcute] hodnoty z\[IAcute]skan\[EAcute] v "Sow" *)(*Poj\[DHacek]me na\[SHacek]e nov\[EAcute] funkce zna\[CHacek]it s mal\[YAcute]mi p\[IAcute]smeny*)
 			NDSolve[ (* \[CapitalRHacek]e\[SHacek]en\[IAcute] ODR *)
 					{
-							\[Theta]''[t] + g/l * Sin[\[Theta][t]] == 0,
+							\[Theta]''[t] + konstg/konstl * Sin[\[Theta][t]] == 0,
 							\[Theta][0] == konst\[Theta]init,
 							\[Theta]'[0] == \[Omega]init
 					},
@@ -71,6 +77,9 @@ Plot[Evaluate[\[Theta][t] /. NumSol],{t,0,4\[Pi]}]
 DifsZeroPoints = Differences[ZeroPoints[[1]]]
 (* Vzdalenosti odpovidaji ctvrtine periody *)(*Polovin\[EHacek] ne? To pak vyjde i stejn\[AAcute] hodnota*)
 TNumSol = 2*Mean[DifsZeroPoints]
+
+
+
 
 
 
